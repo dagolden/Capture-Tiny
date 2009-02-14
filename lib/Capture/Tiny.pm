aa64e1850f7bfb88170a90fb3b111e6d0e6df322
unknown <dgolden@.(none)>
2009-02-13 19:42:18 -0500

    win32 tee working -- added flag files to detect when subprocesses are ready

--- lib/Capture/Tiny.pm
+++ lib/Capture/Tiny.pm
@@ -33,17 +33,9 @@ sub _copy_std {
   open $std[1], ">&STDOUT";
   open $std[2], ">&STDERR";
   return @std;
-#  return (
-#    IO::Handle->new_from_fd( fileno(STDIN ), "r" ),
-#    IO::Handle->new_from_fd( fileno(STDOUT), "w" ),
-#    IO::Handle->new_from_fd( fileno(STDERR), "w" ),
-#  )
 }
 
 sub _open_std {
-#  STDIN ->fdopen( fileno( $_[0] ), 'r' );
-#  STDOUT->fdopen( fileno( $_[1] ), 'w' );
-#  STDOUT->fdopen( fileno( $_[2] ), 'w' );
   open STDIN , "<&" . fileno( $_[0] );
   open STDOUT, ">&" . fileno( $_[1] );
   open STDERR, ">&" . fileno( $_[2] );
@@ -54,21 +46,40 @@ sub _autoflush {
 }
 
 #--------------------------------------------------------------------------#
-# _check_and_clear
+# _win32_wait_for_start
+#--------------------------------------------------------------------------#
+
+sub _win32_wait_for_start {
+}
+#  my @ ) = @_;
+##  while (time - $now < 10) {
+##    my ($out, $err) = _capture_tee( 
+##      sub { print STDOUT "test STDOUT\n"; print STDERR "test STDERR\n" } 
+##    );
+##    $out ||= 'undef';
+##    $err ||= 'undef';
+###    print { $stderr } "Got '$out' and '$err'\n";
+##    Win32::Sleep( 50 );
+##    last if ($out eq "test STDOUT\n") && ($err eq "test STDERR\n");
+##  }
+#  # signal kid process ready to write to STDERR (the capture file) 
+#  kill 1, $_ for ($out_kid, $err_kid);
+#  return;
+#}
+
+#--------------------------------------------------------------------------#
+# _capture_tee()
 #
-# Win32 can take a while to setup the subprocesses.  We want to keep trying
-#--------------------------------------------------------------------------#
-
-sub _check_and_clear {}
-
-#--------------------------------------------------------------------------#
-# _capture()
+# kids for tee must always have cloned STD handle on STDOUT and capture 
+# file on STDERR -- on Windows, need to ensure readiness before enabling
+# tee to capture_file on STDERR.  See _win32_wait_for_start
 #--------------------------------------------------------------------------#
 
 # command to tee output
 my @cmd = ($^X, '-e', 
-  '$SIG{HUP}=sub{exit}; my $buf; while (sysread(STDIN, $buf, 2048)) { ' .
-  'syswrite(STDOUT, $buf); syswrite(STDERR, $buf) }' 
+  '$SIG{HUP}=sub{exit}; my $f=shift; open my $flag, qq{>$f}; print {$flag} $$; close $flag; ' .
+  'my $buf; while (sysread(STDIN, $buf, 2048)) { ' .
+  'syswrite(STDOUT, $buf); syswrite(STDERR, $buf)}' 
 );
 
 sub _capture_tee {
@@ -76,10 +87,10 @@ sub _capture_tee {
   my (@pids, @tees);
   my @copy_of_std = _copy_std();
   
-#  my $stdout_capture = File::Temp::tempfile();
-#  my $stderr_capture = File::Temp::tempfile();
-  my $stdout_capture = IO::File->new( 'stdout.txt', '+>' );
-  my $stderr_capture = IO::File->new( 'stderr.txt', '+>' );
+  my $stdout_capture = File::Temp::tempfile();
+  my $stderr_capture = File::Temp::tempfile();
+#  my $stdout_capture = IO::File->new( 'stdout.txt', '+>' );
+#  my $stderr_capture = IO::File->new( 'stderr.txt', '+>' );
   my ($stdout_tee, $stdout_reader, $stderr_tee, $stderr_reader) =
     map { IO::Handle->new } 1 .. 4;
 
@@ -89,26 +100,27 @@ sub _capture_tee {
     pipe $stderr_reader, $stderr_tee;
     _autoflush( $stdout_tee, $stderr_tee );
     if ( $use_system ) {
+      my @flag_files = map { scalar File::Temp::tmpnam() } 0 .. 1;
       # start STDOUT listener
       _open_std( $stdout_reader, $copy_of_std[1], $stdout_capture );
-      my $out_kid = system(1, @cmd);
+      my $out_kid = system(1, @cmd, $flag_files[0]);
       push @tees, $stdout_tee;
       # start STDERR listener
       _open_std( $stderr_reader, $stderr_capture, $copy_of_std[2] );
-      my $err_kid = system(1, @cmd);
+      my $err_kid = system(1, @cmd, $flag_files[1]);
       push @pids, $out_kid, $err_kid;
       push @tees, $stderr_tee;
-      sleep 2; # let the OS get the processes set up
+      # wait for the OS get the processes set up
+      _open_std( $copy_of_std[0], $stdout_tee, $stderr_tee );
+      1 until -f $flag_files[0] && -f $flag_files[1];
+      unlink $_ for @flag_files;
     }
     else {
       die "fork not implemented yet";
+      _open_std( $copy_of_std[0], $stdout_tee, $stderr_tee );
     }
     $stdout_reader->close;
     $stderr_reader->close;
-#    print {$stdout_tee} "testing out tee\n"; 
-#    print {$stderr_tee} "testing err tee\n"; 
-    # redirect output to kids
-    _open_std( $copy_of_std[0], $stdout_tee, $stderr_tee );
   }
   # otherwise redirect output to capture file
   else {
