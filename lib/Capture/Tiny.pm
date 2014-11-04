0d75d87b12da5576dbdb2b16cba37dc4eb58acdb
David Golden <dagolden@cpan.org>
2014-11-04 06:36:02 -0500

    modernize repo layout; add VERSION to .pm file

--- lib/Capture/Tiny.pm
+++ lib/Capture/Tiny.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 package Capture::Tiny;
 # ABSTRACT: Capture STDOUT and STDERR from Perl, XS or external programs
-# VERSION
+our $VERSION = '0.26';
 use Carp ();
 use Exporter ();
 use IO::Handle ();
