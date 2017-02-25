252e9b77cf87dc9b3d984c242b49ff4e7f6305ec
David Golden <xdg@xdg.me>
2017-02-25 14:25:56 -0500

    After release: bump $VERSION and timestamp Changes

--- lib/Capture/Tiny.pm
+++ lib/Capture/Tiny.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 package Capture::Tiny;
 # ABSTRACT: Capture STDOUT and STDERR from Perl, XS or external programs
-our $VERSION = '0.46';
+our $VERSION = '0.47';
 use Carp ();
 use Exporter ();
 use IO::Handle ();
